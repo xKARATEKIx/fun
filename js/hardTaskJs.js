33e26b94b7d3695f596aaa4bee0fb134296dea1d
Arsen Memetov <arstyle9595@gmail.com>
2022-12-04 20:34:41 -0700

    optimized the code and removed unnecessary lines of code. function for generating output time (hours minutes minutes seconds) was added

--- js/hardTaskJs.js
+++ js/hardTaskJs.js
@@ -1,39 +1,67 @@
 'use strict';
-const timeNode = document.getElementById('time-node'); 
-const getCurrentTimeString = () => {
-   return new Date().toTimeString().replace(/ .*/, '');
+
+const zeroFirstFormat = (value) => {
+    if (value < 10) {
+        value = '0' + value;
+    }
+    return value;
+};
+
+const outputTime = document.querySelector('.time');
+const dateTime = () => {
+    const currentDatetime = new Date();
+    const day = zeroFirstFormat(currentDatetime.getDate());
+    const month = zeroFirstFormat(currentDatetime.getMonth() + 1);
+    const year = currentDatetime.getFullYear();
+    const hours = zeroFirstFormat(currentDatetime.getHours());
+    const minutes = zeroFirstFormat(currentDatetime.getMinutes());
+    const seconds = zeroFirstFormat(currentDatetime.getSeconds());
+
+    return `${day}.${month}.${year} - ${hours}:${minutes}:${seconds}`;
 };
 
 setInterval(
-   () => timeNode.innerHTML = getCurrentTimeString(),
-   1000
+    () => outputTime.innerHTML = dateTime(),
+    1000
 );
 
+const timeNode = document.getElementById('time-node');
+
+
+const timeHtml = document.querySelector('.time');
 const today = new Date();
-const weekDay = { weekday: 'long' };
-const day = {day: 'numeric'};
-const year = {year: 'numeric'};
-const month = {month: 'long'};
+const weekDay = {
+    weekday: 'long'
+};
+const day = {
+    day: 'numeric'
+};
+const year = {
+    year: 'numeric'
+};
+const month = {
+    month: 'long'
+};
 const getWeekDay = today.toLocaleString('en-US', weekDay);
 const getDay = today.toLocaleString('en-US', day);
 const getYear = today.toLocaleString('en-US', year);
 const getMonth = today.toLocaleString('en-US', month);
+const data = new Date();
+const hour = data.getHours();
+const minutes = data.getMinutes();
+const seconds = data.getSeconds();
 const dateHtml = document.querySelector('.date');
 
-dateHtml.innerHTML = `Today is ${getWeekDay}, ${getDay} ${getMonth} ${getYear} year`;
-console.log(getCurrentTimeString());
+const dateTimeSecond = () => {
+    const currentDatetime = new Date();
+    const hours = zeroFirstFormat(currentDatetime.getHours());
+    const minutes = zeroFirstFormat(currentDatetime.getMinutes());
+    const seconds = zeroFirstFormat(currentDatetime.getSeconds());
 
+    return `Today is ${getWeekDay}, ${getDay} ${getMonth} ${getYear} year,  ${hours} hour ${minutes} minutes ${seconds} seconds`;
+};
 
-//=====================================================================
-const secondToday = new Date();
-const outputTime = document.querySelector('.time'); 
-const outputDate = document.querySelector('.second');
-const now = secondToday.toLocaleDateString('en-US');
-outputDate.innerHTML = `${now} -`;
 setInterval(
-    () => outputTime.innerHTML = getCurrentTimeString(),
+    () => dateHtml.innerHTML = dateTimeSecond(),
     1000
- );
-
-
- 
+);
